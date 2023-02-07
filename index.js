375f64c7d0faa78d1910ae3f4b08aeda3422a73b
yunisaliosman <yunis.osman@student.moringaschool.com>
2023-02-07 10:56:41 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 // Write your code in this file!
-const currentUser = 'Sumaya Ahmed Muhumad';
-const welcomeMessage = 'Welcome to Flatbook, ''+ currentUser + '!' ;
-
-const excitedWelcomeMessage = 'Welcome to Flatbook , '.toLocaleUpperCase() +currentUser.toLocaleUpperCase() + '!' ;
-
-const shortGreeting = 'Welcome, '+ currentUser.chart() + '!' ;
+const currentUser = 'Sumaya Ahmed' ;
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toLocaleUpperCase();
+const shortGreeting = `welcome,${currentUser.slice(0,1)}!`;
